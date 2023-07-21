7c05b2d08733c7ef5e2f378310872e22392c04ca
Dmin3 <ekdrnzld@gmail.com>
2023-07-21 16:38:43 +0900

    Initial commit

---
+++ src/main/java/social/donjjul/DonjjulApplication.java
@@ -0,0 +1,13 @@
+package social.donjjul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonjjulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonjjulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/social/donjjul/DonjjulApplicationTests.java
@@ -0,0 +1,13 @@
+package social.donjjul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonjjulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6c0c87dee97a6bf4d5c1ad463d3d6dd48868e848
dmin3 <ekdrnzld@gmail.com>
2023-08-02 16:59:47 +0900

    [BACK] Security 재설정 및 회원가입, 로그인 API 작성

--- backend/src/main/java/social/donjjul/auth/AuthService.java
+++ backend/src/main/java/social/donjjul/auth/AuthService.java
@@ -2,13 +2,20 @@ package social.donjjul.auth;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import social.donjjul.auth.kakao.KakaoAuthService;
 import social.donjjul.auth.kakao.KakaoUserInfoDto;
 import social.donjjul.member.domain.Member;
+import social.donjjul.member.dto.MemberCreateRequest;
 import social.donjjul.member.repository.MemberRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +25,7 @@ import java.util.Optional;
 public class AuthService {
     private final KakaoAuthService kakaoAuthService;
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
     public KakaoUserInfoDto authKakao(String code) {
         String accessToken = kakaoAuthService.getKakaoAccessToken(code);
@@ -31,5 +39,37 @@ public class AuthService {
         }
 
         return kakaoUserInfo;
+    }
+
+    public TokenDto login(String memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow();
+        Authentication authentication = toAuthentication(member);
+        TokenDto tokenDto = tokenProvider.createTokenDto(authentication);
+        return tokenDto;
+    }
+
+    public TokenDto signup(MemberCreateRequest memberCreateRequest) {
+        Optional<Member> findMember = memberRepository.findById(memberCreateRequest.getId());
+
+        if (findMember.isPresent()) {
+            throw new RuntimeException("이미 존재합니다.");
+        }
+
+        Member member = memberRepository.save(
+                Member.createMember(
+                        memberCreateRequest.getId(),
+                        memberCreateRequest.getNickname(),
+                        memberCreateRequest.getProfileImage()
+                )
+        );
+
+        Authentication authentication = toAuthentication(member);
+        TokenDto tokenDto = tokenProvider.createTokenDto(authentication);
+
+        return tokenDto;
+    }
+
+    private Authentication toAuthentication(Member member) {
+        return new UsernamePasswordAuthenticationToken(member.getId(), null, List.of(new SimpleGrantedAuthority(member.getMemberType().toString())));
     }
 }
--- backend/src/main/java/social/donjjul/member/domain/Member.java
+++ backend/src/main/java/social/donjjul/member/domain/Member.java
@@ -17,19 +17,20 @@ public class Member {
     @Column(length = 50)
     private String name;
 
-    private String email;
+    @Column(columnDefinition = "TEXT")
+    private String profileImage;
 
     @Enumerated(value = EnumType.STRING)
     private MemberType memberType;
 
-    public static Member createMember(String id, String name, String email) {
-        return new Member(id, name, email);
+    public static Member createMember(String id, String name, String profileImage) {
+        return new Member(id, name, profileImage);
     }
 
-    public Member(String id, String name, String email) {
+    public Member(String id, String name, String profileImage) {
         this.id = id;
         this.name = name;
-        this.email = email;
+        this.profileImage = profileImage;
         this.memberType = MemberType.USER;
     }
 }
